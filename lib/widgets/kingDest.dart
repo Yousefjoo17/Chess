c5be72711ff02607bc2211f3b79665c7731bbba1
Youssef <yousefyousefyousef335@gmail.com>
2024-06-10 11:19:01 +0300

    kimg moves

--- lib/widgets/kingDest.dart
+++ lib/widgets/kingDest.dart
@@ -1,4 +1,5 @@
 import 'package:chess/constants.dart';
+import 'package:chess/func/is_piece_found_in_pos.dart';
 import 'package:chess/models/DestModel.dart';
 import 'package:chess/models/PieceModel.dart';
 import 'package:chess/widgets/DestWidget.dart';
@@ -11,54 +12,77 @@ class KingDest extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        DestWidget(
-          destModel: DestModel(
-            x: pieceModel.x! + kSQUARE_LENGTH,
-            y: pieceModel.y,
-          ),
-          pieceModel: pieceModel,
-        ),
-        DestWidget(
-          destModel: DestModel(
-            x: pieceModel.x,
-            y: pieceModel.y! + kSQUARE_LENGTH,
-          ),
-          pieceModel: pieceModel,
-        ),
-        DestWidget(
-          destModel: DestModel(
-            x: pieceModel.x! - kSQUARE_LENGTH,
-            y: pieceModel.y,
-          ),
-          pieceModel: pieceModel,
-        ),
-        DestWidget(
-          destModel: DestModel(
-            x: pieceModel.x,
-            y: pieceModel.y! - kSQUARE_LENGTH,
-          ),
-          pieceModel: pieceModel,
-        ),
-        DestWidget(
+        if (isPieceFound(pieceModel.x! + kSQUARE_LENGTH, pieceModel.y!) == null)
+          DestWidget(
             destModel: DestModel(
-                x: pieceModel.x! + kSQUARE_LENGTH,
-                y: pieceModel.y! + kSQUARE_LENGTH),
-            pieceModel: pieceModel),
-        DestWidget(
+              x: pieceModel.x! + kSQUARE_LENGTH,
+              y: pieceModel.y,
+            ),
+            pieceModel: pieceModel,
+          ),
+        //
+        if (isPieceFound(pieceModel.x!, pieceModel.y! + kSQUARE_LENGTH) == null)
+          DestWidget(
             destModel: DestModel(
-                x: pieceModel.x! - kSQUARE_LENGTH,
-                y: pieceModel.y! + kSQUARE_LENGTH),
-            pieceModel: pieceModel),
-        DestWidget(
+              x: pieceModel.x,
+              y: pieceModel.y! + kSQUARE_LENGTH,
+            ),
+            pieceModel: pieceModel,
+          ),
+        //
+        if (isPieceFound(pieceModel.x! - kSQUARE_LENGTH, pieceModel.y!) == null)
+          DestWidget(
             destModel: DestModel(
-                x: pieceModel.x! + kSQUARE_LENGTH,
-                y: pieceModel.y! - kSQUARE_LENGTH),
-            pieceModel: pieceModel),
-        DestWidget(
+              x: pieceModel.x! - kSQUARE_LENGTH,
+              y: pieceModel.y,
+            ),
+            pieceModel: pieceModel,
+          ),
+        //
+        if (isPieceFound(pieceModel.x!, pieceModel.y! - kSQUARE_LENGTH) == null)
+          DestWidget(
             destModel: DestModel(
-                x: pieceModel.x! - kSQUARE_LENGTH,
-                y: pieceModel.y! - kSQUARE_LENGTH),
-            pieceModel: pieceModel),
+              x: pieceModel.x,
+              y: pieceModel.y! - kSQUARE_LENGTH,
+            ),
+            pieceModel: pieceModel,
+          ),
+        //
+        if (isPieceFound(pieceModel.x! + kSQUARE_LENGTH,
+                pieceModel.y! + kSQUARE_LENGTH) ==
+            null)
+          DestWidget(
+              destModel: DestModel(
+                  x: pieceModel.x! + kSQUARE_LENGTH,
+                  y: pieceModel.y! + kSQUARE_LENGTH),
+              pieceModel: pieceModel),
+        //
+        if (isPieceFound(pieceModel.x! - kSQUARE_LENGTH,
+                pieceModel.y! + kSQUARE_LENGTH) ==
+            null)
+          DestWidget(
+              destModel: DestModel(
+                  x: pieceModel.x! - kSQUARE_LENGTH,
+                  y: pieceModel.y! + kSQUARE_LENGTH),
+              pieceModel: pieceModel),
+        //
+        if (isPieceFound(pieceModel.x! + kSQUARE_LENGTH,
+                pieceModel.y! - kSQUARE_LENGTH) ==
+            null)
+          DestWidget(
+              destModel: DestModel(
+                  x: pieceModel.x! + kSQUARE_LENGTH,
+                  y: pieceModel.y! - kSQUARE_LENGTH),
+              pieceModel: pieceModel),
+        //
+        if (isPieceFound(pieceModel.x! - kSQUARE_LENGTH,
+                pieceModel.y! - kSQUARE_LENGTH) ==
+            null)
+          DestWidget(
+              destModel: DestModel(
+                  x: pieceModel.x! - kSQUARE_LENGTH,
+                  y: pieceModel.y! - kSQUARE_LENGTH),
+              pieceModel: pieceModel),
       ],
     );
   }
